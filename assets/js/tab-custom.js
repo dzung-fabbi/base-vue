291d05f2006cc42c897acdd95e8365291f833db9
dong.c5 <dong.c5@amela.vn>
2021-05-24 22:00:34 +0700

    update design

--- assets/js/tab-custom.js
+++ assets/js/tab-custom.js
@@ -6,12 +6,15 @@ $(".tab-slider--nav li").click(function () {
     $(".tab-slider--body").hide();
     let activeTab = $(this).attr("rel");
     $("#" + activeTab).fadeIn();
-    if ($(this).attr("rel") === "tab2") {
-        $('.tab-slider--tabs').addClass('slide');
-        $(".tab-slider--body:last-child").show();
-    } else {
+    if ($(this).attr("rel") === "tab1") {
         $(".tab-slider--body:first").show();
         $('.tab-slider--tabs').removeClass('slide');
+    } else if ($(this).attr("rel") === "tab2") {
+        $('.tab-slider--tabs').addClass('slide');
+        $(".tab-slider--body:nth-child(2)").show();
+    } else {
+        $('.tab-slider--tabs').addClass('slide');
+        $(".tab-slider--body:last-child").show();
     }
     $(".tab-slider--nav li").removeClass("active");
     $(this).addClass("active");
