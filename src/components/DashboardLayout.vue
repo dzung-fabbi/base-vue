36b297ca4939df4dbcd6033fffc2542c6bf2c155
DungVV-Fabbi <dungvv@fabbi.com.vn>
2021-05-29 12:12:12 +0700

    fix navbar

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -17,52 +17,90 @@
         <img :src="require('@/assets/image/logo.png')" alt="logo" class="logo">
       </router-link>
       <ul class="nav flex-column vertical-nav">
-        <li class="nav-item sub-menu">
-          <router-link :to="{ name: 'Home' }">
-            <DashboardIcon />
-            <span>ダッシュボード</span>
-          </router-link>
-        </li>
-        <li class='nav-item sub-menu'>
+        <router-link tag="li" class="nav-item" :to="{ name: 'Home' }">
+          <DashboardIcon />
+          <span>ダッシュボード</span>
+        </router-link>
+        <li 
+          class='nav-item sub-menu' 
+          :class="isExpandUser ? 'active' : ''"
+          @click="isExpandUser = !isExpandUser"
+        >
           <a>
             <UserIcon />
             <span>ユーザー管理</span>
-            <i class="fa fa-chevron-up right" aria-hidden="true"></i>
+            <b-icon class="icon right" 
+              :icon="isExpandUser ? 'chevron-down' : 'chevron-up'" 
+              aria-hidden="true"
+            ></b-icon>
           </a>
           <ul class="child-item">
-            <li><router-link :to="{ name:'UserList' }">ユーザー一覧</router-link></li>
-            <li><router-link :to="{ name: 'UserReportList' }">ブロック一覧管理</router-link></li>
-            <li><router-link :to="{ name: 'UserBlockList' }">ブロック一覧管理</router-link></li>
+            <router-link 
+              tag="li" :to="{ name:'UserList' }"
+              @click.native="onClickSub($event, 'isExpandUser')"
+            >ユーザー一覧</router-link>
+            <router-link 
+              tag="li" :to="{ name: 'UserReportList' }"
+              @click.native="onClickSub($event, 'isExpandUser')"
+            >ブロック一覧管理</router-link>
+            <router-link 
+              tag="li" :to="{ name: 'UserBlockList' }"
+              @click.native="onClickSub($event, 'isExpandUser')"
+            >ブロック一覧管理</router-link>
           </ul>
         </li>
-        <li class="nav-item sub-menu">
+        <li 
+          class="nav-item sub-menu"
+          :class="isExpandRevenue ? 'active' : ''"
+          @click="isExpandRevenue = !isExpandRevenue"
+        >
           <a>
             <RevenueIcon />
             <span>売上管理</span>
-            <i class="fa fa-chevron-up right" aria-hidden="true"></i>
+            <b-icon class="icon right" 
+              :icon="isExpandRevenue ? 'chevron-down' : 'chevron-up'" 
+              aria-hidden="true"
+            ></b-icon>
           </a>
           <ul class="child-item">
-            <li><router-link :to="{ name: 'RevenueSystem' }">総売上</router-link></li>
-            <li><router-link :to="{ name: 'RevenueUser' }">収入管理</router-link></li>
+            <router-link 
+              tag="li" :to="{ name: 'RevenueSystem' }"
+              @click.native="onClickSub($event, 'isExpandRevenue')"
+            >総売上</router-link>
+            <router-link 
+              tag="li" :to="{ name: 'RevenueUser' }"
+              @click.native="onClickSub($event, 'isExpandRevenue')"
+            >収入管理</router-link>
           </ul>
         </li>
-        <li class="nav-item sub-menu">
+        <li 
+          class="nav-item sub-menu"
+          :class="isExpandPayment ? 'active' : ''"
+          @click="isExpandPayment = !isExpandPayment"
+        >
           <a>
             <PaymentIcon />
             <span>決済管理</span>
-            <i class="fa fa-chevron-up right" aria-hidden="true"></i>
+            <b-icon class="icon right" 
+              :icon="isExpandPayment ? 'chevron-down' : 'chevron-up'" 
+              aria-hidden="true"
+            ></b-icon>
           </a>
           <ul class="child-item">
-            <li><router-link :to="{ name: 'PaymentList' }">決済履歴</router-link></li>
-            <li><router-link :to="{ name: 'PaymentSetting' }">設定</router-link></li>
+            <router-link 
+              tag="li" :to="{ name: 'PaymentList' }"
+              @click.native="onClickSub($event, 'isExpandPayment')"
+            >決済履歴</router-link>
+            <router-link 
+              tag="li" :to="{ name: 'PaymentSetting' }"
+              @click.native="onClickSub($event, 'isExpandPayment')"
+            >設定</router-link>
           </ul>
         </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'LiveManagement' }">
-            <LiveIcon />
-            <span>ライブ管理</span>
-          </router-link>
-        </li>
+        <router-link tag="li" class="nav-item" :to="{ name: 'LiveManagement' }">
+          <LiveIcon />
+          <span>ライブ管理</span>
+        </router-link>
       </ul>
     </nav>
     <div class="main background-f6f9fa">
@@ -91,6 +129,19 @@ export default {
   data()  {
     return {
       user: null,
+      isExpandUser: false,
+      isExpandRevenue: false,
+      isExpandPayment: false
+    }
+  },
+  mounted() {
+    const path = window.location.pathname;
+    if (path.startsWith('/users')) {
+      this.isExpandUser = true;
+    } else if (path.startsWith('/revenues')) {
+      this.isExpandRevenue = true;
+    } else if (path.startsWith('/payments')) {
+      this.isExpandPayment = true;
     }
   },
   created() {
@@ -103,6 +154,10 @@ export default {
       this.$router.push({ name: "Login" });
       this.$root.$refs.loading.finish();
     },
+    onClickSub(event, name) {
+      event.stopPropagation();
+      this[name] = true;
+    }
   }
 };
 </script>
