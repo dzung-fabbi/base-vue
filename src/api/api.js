566d78145204045d8992bfc988f79262aa159392
dong.c5 <dong.c5@amela.vn>
2021-05-30 13:29:22 +0700

    change api route

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
-export const LOGIN_USER_API = "/api/v1/admin/login";
-export const GET_INFO_ACCOUNT = "/api/v1/admin/user-info";
-export const GET_LIST_USER = "/api/v1/admin/user-list";
-export const GET_STATISTICAL_USER = "/api/v1/admin/user-statistical";
-export const GET_USER_DETAIL = user_id => `/api/v1/admin/user-info/${user_id}`;
-export const UPDATE_USER_INFO = user_id => `/api/v1/admin/user-info/${user_id}`;
-export const BLOCK_USER = user_id => `/api/v1/admin/block-user/${user_id}`;
-export const GET_BLOCK_USER_LIST = "/api/v1/admin/user-list";
-export const DELETE_USER = user_id => `/api/v1/admin/delete-user/${user_id}`;
+export const LOGIN_USER_API = "/api/v1/cms/login";
+export const GET_INFO_ACCOUNT = "/api/v1/cms/user-info";
+export const GET_LIST_USER = "/api/v1/cms/user-list";
+export const GET_STATISTICAL_USER = "/api/v1/cms/user-statistical";
+export const GET_USER_DETAIL = user_id => `/api/v1/cms/user-info/${user_id}`;
+export const UPDATE_USER_INFO = user_id => `/api/v1/cms/user-info/${user_id}`;
+export const BLOCK_USER = user_id => `/api/v1/cms/block-user/${user_id}`;
+export const GET_BLOCK_USER_LIST = "/api/v1/cms/user-list";
+export const DELETE_USER = user_id => `/api/v1/cms/delete-user/${user_id}`;
