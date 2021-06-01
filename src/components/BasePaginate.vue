f61811d4bd0ec341585943368f70458dab278021
dong.c5 <dong.c5@amela.vn>
2021-06-02 00:39:33 +0700

    change record count

--- src/components/BasePaginate.vue
+++ src/components/BasePaginate.vue
@@ -26,6 +26,7 @@
 <script>
 import NextPageIcon from "@/components/Icon/NextPageIcon";
 import PreviousPageIcon from "@/components/Icon/PreviousPageIcon";
+import {PER_PAGE_NUMBER} from '@/utils/const';
 
 export default {
   components: {
@@ -45,18 +46,18 @@ export default {
     perPage: {
       type: Number,
       required: true,
-      default: 10,
+      default: PER_PAGE_NUMBER,
     },
     totalRecord: {
       type: Number,
       required: true,
-      default: 10,
+      default: PER_PAGE_NUMBER,
     }
   },
   data() {
     return {
       from_record: 1,
-      to_record: 10,
+      to_record: PER_PAGE_NUMBER,
     }
   },
   computed: {
