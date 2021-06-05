c1488dddaafebad71e57800a18365a2435358c2b
DungVV-Fabbi <dungvv@fabbi.com.vn>
2021-06-05 22:29:28 +0700

    merge_api: dashboard

---
+++ src/api/dashboard.js
@@ -0,0 +1,4 @@
+export const GET_DASHBOARD_INCOME = "/api/v1/cms/dashboard/income";
+export const GET_DASHBOARD_DISTRIBUTOR = "/api/v1/cms/dashboard/livestreams";
+export const GET_DASHBOARD_REGISTER = "/api/v1/cms/dashboard/user-register";
+export const GET_DASHBOARD_ANALYTIC = "/api/v1/cms/dashboard/analytic";
